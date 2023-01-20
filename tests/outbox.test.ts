679a24092846ca4ba8e1cd94bcb2d9f8f7b560d9
Tomás Migone <tomas@edgeandnode.com>
2023-01-20 16:05:45 -0300

    feat: add test
    Signed-off-by: Tomás Migone <tomas@edgeandnode.com>

--- tests/outbox.test.ts
+++ tests/outbox.test.ts
@@ -15,6 +15,7 @@ import {
   BigInt,
   ByteArray,
 } from "@graphprotocol/graph-ts";
+import { calculateSubmitRetryableId } from '../src/helpers/bridge'
 
 describe("Describe entity assertions", () => {
   test("test", () => {
@@ -59,25 +60,25 @@ describe("Describe entity assertions", () => {
     //   []
     // );
     // log.info(`dog is ${RLPEncode(Bytes.fromUTF8(""))}`, []);
-    // let input2: ByteArray[] = [
-    //   Bytes.fromHexString("0x066EED"),
-    //   Bytes.fromHexString(
-    //     "0x0000000000000000000000000000000000000000000000000000000000007A12"
-    //   ),
-    //   Bytes.fromHexString("0xd940f7b51C3e593D709BA3de1B3a0d233d1DFDb2"),
-    //   Bytes.fromHexString("0x020ECA945D"),
-    //   Bytes.fromHexString("0x7894bf4ae940"),
-    //   Bytes.fromHexString("0x11e1a300"),
-    //   Bytes.fromHexString("0x01b22d"),
-    //   Bytes.fromHexString("0xef2757855d2802bA53733901F90C91645973f743"),
-    //   Bytes.fromHexString("0x"),
-    //   Bytes.fromHexString("0xEfc519BEd6a43a14f1BBBbA9e796C4931f7A5540"),
-    //   Bytes.fromHexString("0x5a41144b4240"),
-    //   Bytes.fromHexString("0xEfc519BEd6a43a14f1BBBbA9e796C4931f7A5540"),
-    //   Bytes.fromHexString(
-    //     "0x2e567b360000000000000000000000005c946740441c12510a167b447b7de565c20b9e3c000000000000000000000000efc519bed6a43a14f1bbbba9e796c4931f7a5540000000000000000000000000efc519bed6a43a14f1bbbba9e796c4931f7a5540000000000000000000000000000000000000000000000000016345785d8a000000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000000"
-    //   ),
-    // ];
-    // log.info(`retryableTicketId is ${calculateSubmitRetryableId(input2)}`, []);
+    let input2: ByteArray[] = [
+      Bytes.fromHexString("0x066EED"),
+      Bytes.fromHexString(
+        "0x0000000000000000000000000000000000000000000000000000000000007A12"
+      ),
+      Bytes.fromHexString("0xd940f7b51C3e593D709BA3de1B3a0d233d1DFDb2"),
+      Bytes.fromHexString("0x020ECA945D"),
+      Bytes.fromHexString("0x7894bf4ae940"),
+      Bytes.fromHexString("0x11e1a300"),
+      Bytes.fromHexString("0x01b22d"),
+      Bytes.fromHexString("0xef2757855d2802bA53733901F90C91645973f743"),
+      Bytes.fromHexString("0x"),
+      Bytes.fromHexString("0xEfc519BEd6a43a14f1BBBbA9e796C4931f7A5540"),
+      Bytes.fromHexString("0x5a41144b4240"),
+      Bytes.fromHexString("0xEfc519BEd6a43a14f1BBBbA9e796C4931f7A5540"),
+      Bytes.fromHexString(
+        "0x2e567b360000000000000000000000005c946740441c12510a167b447b7de565c20b9e3c000000000000000000000000efc519bed6a43a14f1bbbba9e796c4931f7a5540000000000000000000000000efc519bed6a43a14f1bbbba9e796c4931f7a5540000000000000000000000000000000000000000000000000016345785d8a000000000000000000000000000000000000000000000000000000000000000000a00000000000000000000000000000000000000000000000000000000000000000"
+      ),
+    ];
+    log.info(`retryableTicketId is ${calculateSubmitRetryableId(input2)}`, []);
   });
 });
